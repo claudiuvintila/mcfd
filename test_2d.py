152c6bb75224aa19b75480b65d97d4bd26925163
claudiuvintila <claudiu.vintila@gmail.com>
2020-11-17 22:31:43 +0200

    cleanup

--- test_2d.py
+++ test_2d.py
@@ -6,7 +6,6 @@ from PIL import Image
 if __name__ == '__main__':
     img_tensor, clusters = mcfd.generate_tensor([1024, 1024], cluster_count=25, cluster_radius=30, low=0, high=255, eps=55)
     img_tensor = 255 - img_tensor
-    print(img_tensor.shape)
 
     # Creates PIL image
     img = Image.fromarray(img_tensor)
